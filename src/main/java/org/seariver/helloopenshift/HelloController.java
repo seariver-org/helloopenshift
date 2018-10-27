9f8562f53b6af55fb014f772225f9f2a8009a2db
Mario Rezende <mariorez@gmail.com>
2018-10-27 02:35:35 -0300

    first commit

---
+++ src/main/java/org/seariver/helloopenshift/HelloController.java
@@ -0,0 +1,24 @@
+package org.seariver.helloopenshift;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${hello.message}")
+    private String message;
+
+    @GetMapping(value = "hello", produces = "application/json")
+    public ResponseEntity hello() {
+
+        String json = String.format("{\"appName\": \"%s\", \"hello\": \"%s\"}", appName, message);
+
+        return ResponseEntity.ok().body(json);
+    }
+}
---
+++ src/main/java/org/seariver/helloopenshift/HelloOpenShiftApplication.java
@@ -0,0 +1,12 @@
+package org.seariver.helloopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloOpenShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloOpenShiftApplication.class, args);
+	}
+}
